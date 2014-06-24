43b2d87a875ed02b74a2968560d2e76d1431193e
Vishal Vijay <0vishalvijay0@gmail.com>
2014-06-24 23:53:53 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VisitingCardServer::Application.config.secret_key_base = 'edad34c0aa199b1cb00a0c0c5c03b4ae13de4b51c5b60182ee6d07cc2e46630e2e5ada8abeb8ae47ea888b91613bd3370d63c06e93ca8744eb88da5bf37036d8'
