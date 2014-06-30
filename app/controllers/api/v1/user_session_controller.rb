77b226a869e3c036dd632bcbbf456cadb0209e21
Vishal Vijay <0vishalvijay0@gmail.com>
2014-06-30 22:40:24 +0530

    login code added for debugging

--- app/controllers/api/v1/user_session_controller.rb
+++ app/controllers/api/v1/user_session_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UserSessionController < Devise::SessionsController
-  #  def create
-  #   self.resource = User.find_by_email(params[:user][:email])
-  #   set_flash_message(:notice, :signed_in) if is_flashing_format?
-  #   sign_in(resource_name, resource)
-  #   yield resource if block_given?
-  #   respond_with resource, location: after_sign_in_path_for(resource)
-  # end
+   def create
+    self.resource = User.find_by_email(params[:user][:email])
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
 end
