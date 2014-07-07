29f6869407039ee9180fa5bdd5ac58080433dfe7
Vishal Vijay <0vishalvijay0@gmail.com>
2014-07-08 00:48:37 +0530

    gcm data fix

--- lib/push_notification.rb
+++ lib/push_notification.rb
@@ -4,7 +4,7 @@ class PushNotification
   def self.send ids, data
     if ids.length > 0
       gcm = GCM.new(Settings.gcm_api_key)
-      response = gcm.send_notification(ids, data)
+      response = gcm.send_notification(ids, {data: data})
     end
   end
 end
