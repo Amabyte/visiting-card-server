69c904acb5f825dc70ff34414239a968f6711925
Vishal Vijay <0vishalvijay0@gmail.com>
2014-08-07 03:16:37 +0530

    fix for getting different image

--- app/models/visiting_card_data.rb
+++ app/models/visiting_card_data.rb
@@ -1,6 +1,6 @@
 class VisitingCardData < ActiveRecord::Base
   validates_presence_of :visiting_card, :key, :value
-  has_attached_file :image, :path => ":rails_root/data/images/vc/:visiting_card_id/vcd/:key.:extension"
+  has_attached_file :image, :path => ":rails_root/data/images/vcd/:id.:extension"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   belongs_to :visiting_card
 
